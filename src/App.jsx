f57f95e5aeb35fe74c088a59b23b8cddd0015951
yo-shi-mi <erin001tw@gmail.com>
2025-10-07 01:34:45 +0800

    Enhance main layout with overflow handling

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <div className="relative font-mono min-h-screen bg-[#E4EAF2] lg:bg-[url('/images/background.svg')] lg:bg-no-repeat lg:bg-top lg:bg-[length:100%_auto] overflow-hidden">
       <div className="mx-auto w-full max-w-[375px] min-w-[375px] lg:max-w-none lg:min-w-0 bg-[url('/images/background.svg')] bg-no-repeat bg-top bg-[length:100%_auto] lg:bg-none">
         <Header />
-        <main>
+        <main className="overflow-x-hidden">
           <Hero />
           <Features />
           <Waitlist />
