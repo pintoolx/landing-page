6a51fa588e824ece8d0442eddbf406ea5df22fd9
yo-shi-mi <erin001tw@gmail.com>
2025-10-07 01:18:51 +0800

    Refactor App layout and enforce minimum width for desktop

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="relative font-mono min-h-screen bg-[#E4EAF2] lg:bg-[url('/images/background.svg')] lg:bg-no-repeat lg:bg-top lg:bg-[length:100%_auto] overflow-hidden">
-      <Header />
-      <main className="bg-[url('/images/background.svg')] bg-no-repeat bg-top bg-[length:100%_auto] lg:bg-none">
-        <Hero />
-        <Features />
-        <Waitlist />
-      </main>
-      <Footer />
+      <div className="mx-auto w-full max-w-[375px] min-w-[375px] lg:max-w-none lg:min-w-0 bg-[url('/images/background.svg')] bg-no-repeat bg-top bg-[length:100%_auto] lg:bg-none">
+        <Header />
+        <main>
+          <Hero />
+          <Features />
+          <Waitlist />
+        </main>
+        <Footer />
+      </div>
     </div>
   );
 }
